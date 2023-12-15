48ea0a6259d626296a1ac1cd6e5e16572e17dcfc
Francisco Carrero Nogales <130976226+CarriHub@users.noreply.github.com>
2023-12-15 13:44:03 +0100

    Primer comit

---
+++ php/conexion_be.php
@@ -0,0 +1,13 @@
+<?php
+
+$conexion = mysqli_connect("localhost", "mycarri542d", "KQM8x5rh", "inicio41j");
+
+/*/
+if ($conexion){
+    echo "conectado";
+}else{
+        echo "no conectado";
+}
+*/
+
+?>
